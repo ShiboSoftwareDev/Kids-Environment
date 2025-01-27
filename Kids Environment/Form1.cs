cb0e97e5359e194658fc122ac14f7cabadc29f72
Ahmed Shebani <ahmedsheb2003@yahoo.com>
2025-01-27 11:10:02 +0200

    cleanup

--- Kids Environment/Form1.cs
+++ Kids Environment/Form1.cs
@@ -5,11 +5,10 @@ namespace KidsGameEnvironment
 {
     public partial class MainForm : Form
     {
-        //private Panel mainPanel;
-        //private Panel game1Panel;
-        //private Panel game2Panel;
-        //private Panel game3Panel;
-        //private Button btnReturn;
+        private Panel mainPanel;
+        private Panel game1Panel;
+        private Panel game2Panel;
+        private Panel game3Panel;
 
         public MainForm()
         {
@@ -18,81 +17,23 @@ namespace KidsGameEnvironment
 
         private void Init()
         {
-            this.btnGame1 = new System.Windows.Forms.Button();
-            this.btnGame2 = new System.Windows.Forms.Button();
-            this.btnGame3 = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
             this.mainPanel = new System.Windows.Forms.Panel();
             this.game1Panel = CreateGamePanel("game1Panel");
             this.game2Panel = CreateGamePanel("game2Panel");
             this.game3Panel = CreateGamePanel("game3Panel");
-            this.btnReturn = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnGame1
-            // 
-            this.btnGame1.Location = new System.Drawing.Point(175, 262);
-            this.btnGame1.Name = "btnGame1";
-            this.btnGame1.Size = new System.Drawing.Size(150, 75);
-            this.btnGame1.TabIndex = 0;
-            this.btnGame1.Text = "Game 1";
-            this.btnGame1.UseVisualStyleBackColor = true;
-            this.btnGame1.Click += new System.EventHandler(this.btnGame1_Click);
-            // 
-            // btnGame2
-            // 
-            this.btnGame2.Location = new System.Drawing.Point(335, 262);
-            this.btnGame2.Name = "btnGame2";
-            this.btnGame2.Size = new System.Drawing.Size(150, 75);
-            this.btnGame2.TabIndex = 1;
-            this.btnGame2.Text = "Game 2";
-            this.btnGame2.UseVisualStyleBackColor = true;
-            this.btnGame2.Click += new System.EventHandler(this.btnGame2_Click);
-            // 
-            // btnGame3
-            // 
-            this.btnGame3.Location = new System.Drawing.Point(495, 262);
-            this.btnGame3.Name = "btnGame3";
-            this.btnGame3.Size = new System.Drawing.Size(150, 75);
-            this.btnGame3.TabIndex = 2;
-            this.btnGame3.Text = "Game 3";
-            this.btnGame3.UseVisualStyleBackColor = true;
-            this.btnGame3.Click += new System.EventHandler(this.btnGame3_Click);
-            // 
-            // btnExit
-            // 
-            this.btnExit.Location = new System.Drawing.Point(655, 262);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(150, 75);
-            this.btnExit.TabIndex = 3;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = false;
-            this.btnExit.BackColor = System.Drawing.Color.Red;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // mainPanel
             // 
-            this.mainPanel.Controls.Add(this.btnGame1);
-            this.mainPanel.Controls.Add(this.btnGame2);
-            this.mainPanel.Controls.Add(this.btnGame3);
-            this.mainPanel.Controls.Add(this.btnExit);
+            this.mainPanel.Controls.Add(CreateButton("btnGame1", "Game 1", new System.Drawing.Point(175, 262), new System.EventHandler(this.btnGame1_Click)));
+            this.mainPanel.Controls.Add(CreateButton("btnGame2", "Game 2", new System.Drawing.Point(335, 262), new System.EventHandler(this.btnGame2_Click)));
+            this.mainPanel.Controls.Add(CreateButton("btnGame3", "Game 3", new System.Drawing.Point(495, 262), new System.EventHandler(this.btnGame3_Click)));
+            this.mainPanel.Controls.Add(CreateButton("btnExit", "Exit", new System.Drawing.Point(655, 262), new System.EventHandler(this.btnExit_Click), System.Drawing.Color.Red));
             this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainPanel.Location = new System.Drawing.Point(0, 0);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(1000, 600);
             this.mainPanel.TabIndex = 4;
-            // 
-            // btnReturn
-            // 
-            this.btnReturn.Location = new System.Drawing.Point(12, 12);
-            this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(100, 50);
-            this.btnReturn.TabIndex = 8;
-            this.btnReturn.Text = "Return";
-            this.btnReturn.UseVisualStyleBackColor = false;
-            this.btnReturn.BackColor = System.Drawing.Color.Red;
-            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
-            this.btnReturn.Visible = false;
             // 
             // MainForm
             // 
@@ -101,7 +42,6 @@ namespace KidsGameEnvironment
             this.Controls.Add(this.game1Panel);
             this.Controls.Add(this.game2Panel);
             this.Controls.Add(this.game3Panel);
-            this.Controls.Add(this.btnReturn);
             this.Name = "MainForm";
             this.Text = "Kids Game Environment";
             this.ResumeLayout(false);
@@ -139,7 +79,6 @@ namespace KidsGameEnvironment
             game1Panel.Visible = false;
             game2Panel.Visible = false;
             game3Panel.Visible = false;
-            btnReturn.Visible = true;
 
             panel.Visible = true;
         }
@@ -172,14 +111,27 @@ namespace KidsGameEnvironment
             return panel;
         }
 
-        private System.Windows.Forms.Button btnGame1;
-        private System.Windows.Forms.Button btnGame2;
-        private System.Windows.Forms.Button btnGame3;
-        private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.Panel mainPanel;
-        private System.Windows.Forms.Panel game1Panel;
-        private System.Windows.Forms.Panel game2Panel;
-        private System.Windows.Forms.Panel game3Panel;
-        private System.Windows.Forms.Button btnReturn;
+        private Button CreateButton(string name, string text, System.Drawing.Point location, EventHandler clickEvent, System.Drawing.Color? backColor = null)
+        {
+            Button button = new Button
+            {
+                Location = location,
+                Name = name,
+                Size = new System.Drawing.Size(150, 75),
+                Text = text,
+                UseVisualStyleBackColor = true
+            };
+
+            if (backColor.HasValue)
+            {
+                button.BackColor = backColor.Value;
+            }
+
+            button.Click += clickEvent;
+
+            return button;
+        }
+
     }
 }
+
